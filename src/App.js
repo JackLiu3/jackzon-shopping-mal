08ac61cedba989cd2f9413b80a9671f3fa32035f
JackLiu3 <86380806+JackLiu3@users.noreply.github.com>
2021-08-03 21:06:43 -0500

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import "./styles.css";
+
+export default function App() {
+  return (
+    <div>
+      <div id="title" className="line1">
+        <h3>Jackazon Shopping Mall</h3>
+        <img className="cart-img" src="images/cart.png" />
+      </div>
+      <div></div>
+    </div>
+  );
+}
